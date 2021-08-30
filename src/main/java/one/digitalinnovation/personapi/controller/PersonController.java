47d26a48fb26bc3b0b728d62b77bcb0b512cc664
root <root@localhost.localdomain>
2021-08-30 14:21:07 -0300

    Developed get person by ID

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,8 +1,10 @@
 package one.digitalinnovation.personapi.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(value = "API REST PERSON")
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
@@ -22,16 +25,23 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @ApiOperation(value = "Create person")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
+        return personService.createPerson(personDTO);
+    }
+
     @ApiOperation(value = "List all people")
     @GetMapping
     public List<PersonDTO> listAll(){
         return personService.listAll();
     }
 
-    @ApiOperation(value = "Create person")
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
-        return personService.createPerson(personDTO);
+    @ApiOperation(value = "Return person by id")
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
     }
 }
